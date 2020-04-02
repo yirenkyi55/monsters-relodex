721521c2a47925a05f08e44aee099b6327bd70fc
Emmanuel Yirenkyi <fb_emmanuel@outlook.com>
2020-04-02 14:26:26 +0000

    Updates

---
+++ precache-manifest.be49ac6ee109d265d3b859c5c656c7ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "656e653bfaaf98f0536779a6ebe5c638",
+    "url": "/monsters-relodex/index.html"
+  },
+  {
+    "revision": "87317a8c37a108c6b7e4",
+    "url": "/monsters-relodex/static/css/main.0dafe6f1.chunk.css"
+  },
+  {
+    "revision": "d064ea85da7c2e271b4d",
+    "url": "/monsters-relodex/static/js/2.d9d4b753.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-relodex/static/js/2.d9d4b753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87317a8c37a108c6b7e4",
+    "url": "/monsters-relodex/static/js/main.7fc267f7.chunk.js"
+  },
+  {
+    "revision": "c3ba1f1cd420938ccc03",
+    "url": "/monsters-relodex/static/js/runtime-main.92953fd5.js"
+  }
+]);
